a3ae6789a3e5835d7170990b052d1fe905530f5e
ASSiddik01 <assiddik001@gmail.com>
2021-06-02 23:30:10 +0600

    add number system file

---
+++ 01. PHP Basic Subject/09. Different number system/09. Different number system.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Octal start with 0
+ * octal number is 0-7 
+ * hexadecimal start with 0x
+ * Binary %b
+ * Octal %o
+ * Hexadecimal %x
+ * Decimal %d
+ */
+
+
+printf ("The decimal number %d is eual to binary %b\n",33,33);
+printf ("The binary number %b is eual to decimal %d\n",40,40);
+printf ("The binary number %b is eual to Hexadecimal %X\n",40,40);
+printf ("The decimal number %d is eual to octal %o\n",40,40);
+
