8c3d3710488fc01750c0e0a5347517263f6cf555
ASSiddik01 <assiddik001@gmail.com>
2021-06-02 23:14:32 +0600

    add more about php

---
+++ 01. PHP Basic Subject/08. More about math/08. More about math.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * $result = 10 +2
+ * varialble = 10 operator 2
+ */
+$a = 5;
+$x = $y = $a;
+
+echo $x;
+echo PHP_EOL;
+echo $y;
+echo PHP_EOL;
+
+$number = 6;
+$number ++;
+$number ++;
+$number ++;
+$number ++;
+
+echo $number;
+echo PHP_EOL;
+
+
+$n =5;
+$m = $n++; //$m=$n and $n=$n+1
+echo $m."\n".$n;
+echo PHP_EOL;
+
+
+$n =5;
+$m = ++$n; //$n=$n+1 and $m=$n
+echo $m."\n".$n;
+
