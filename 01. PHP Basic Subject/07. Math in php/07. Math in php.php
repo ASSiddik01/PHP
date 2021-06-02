170a5e414cad3c1244879e87a590cd2e8e0683c7
ASSiddik01 <assiddik001@gmail.com>
2021-06-02 22:43:17 +0600

    add math is php

---
+++ 01. PHP Basic Subject/07. Math in php/07. Math in php.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * add +
+ * Subtraction -
+ * Multiply *
+ * Division /
+ * Remainder % this called modulas
+ * Artithmatic Operator + - * / %
+ * % modulas
+ */
+$number = 15;
+// $number = $number +20;
+$number +=15; // This means $number = $number +20;
+echo $number;
+echo PHP_EOL;
+
+$subtraction = $number-2;
+echo $subtraction;
+echo PHP_EOL;
+
+$multiply = 5*5;
+echo $multiply;
+echo PHP_EOL;
+
+$division = 12/5;
+echo $division;
+echo PHP_EOL;
+
+$remainder = 15%4;
+echo $remainder;
+echo PHP_EOL;
+
+$result = (10-2)*5; // php follow the BODMAS rules
+echo $result;
+echo PHP_EOL;
+
