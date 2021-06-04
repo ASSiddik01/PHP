22cedc1536ee627d59d422aa801bb7bcf623fcf4
ASSiddik01 <assiddik001@gmail.com>
2021-06-04 15:47:32 +0600

    modified complex if else ckondition

--- 01. PHP Basic Subject/13. Complex if else condition/13. Complex if else condition.php
+++ 01. PHP Basic Subject/13. Complex if else condition/13. Complex if else condition.php
@@ -1,13 +1,22 @@
 <?php
 // Leap year calculation
 
-$year = 1900;
+$year = 1988;
 
 if ( 0 == $year % 4 && 0 == $year % 100 && 0 == $year % 400 ) {
     echo "{$year} is a leap year";
 } elseif ( 0 == $year % 4 && 0 == $year % 100 ) {
     echo "{$year} is not a leap year";
-} elseif ( 0 == $year % 0 ) {
+} elseif ( 0 == $year % 4 ) {
+    echo "{$year} is a leap year";
+} else {
+    echo "{$year} is not a leap year";
+}
+echo PHP_EOL;
+
+// shortcut
+
+if ( 0 == $year % 4 && (400 == $year % 400 || 0 != $year % 100 )) {
     echo "{$year} is a leap year";
 } else {
     echo "{$year} is not a leap year";
