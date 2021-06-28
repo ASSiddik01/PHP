930b8af651cd504410c1cb5371e9072de03f4970
Shama <assiddik001@gmail.com>
2021-06-28 11:11:53 +0600

    even odd calculation by function

--- 02. Function/01. How to right functions/functions.php
+++ 02. Function/01. How to right functions/functions.php
@@ -1,1 +1,9 @@
 <?php
+
+function isEven( $n ) {
+
+    if ( 0 == $n % 2 ) {
+        return true;
+    }
+    return false;
+}
--- 02. Function/01. How to right functions/work.php
+++ 02. Function/01. How to right functions/work.php
@@ -1,1 +1,11 @@
 <?php
+
+include_once "functions.php";
+
+$x = 13;
+
+if ( isEven( $x ) ) {
+    echo "{$x} is an even number";
+} else {
+    echo "{$x} is an odd number";
+}
