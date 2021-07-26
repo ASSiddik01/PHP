796e2f46f60ebf2e4432195ac1e8ad61c5e4a0be
Shama <assiddik001@gmail.com>
2021-07-26 21:07:44 +0600

    return type set in function

---
+++ 02. Function/04. Return type fix in function/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+//it you set the return type 
+//function "function name"( parameter ): "return type" 
+
+
+function sum( int $x, int $y, int $z ): int {
+    return $x + $y + $z;
+
+}
---
+++ 02. Function/04. Return type fix in function/work.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once "functions.php";
+
+echo sum(10,6,8);
